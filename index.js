2b063ae7bac6cf641cf29fb8ceb967441dadfc7e
Pakdee H <pakdee.hn@outlook.com>
2024-12-10 01:05:49 +0700

    [+] Add tooltips

---
+++ index.js
@@ -0,0 +1,14 @@
+let myBirth = new Date("08/11/1998");
+//calculate month difference from current date in time  
+let month_diff = Date.now() - myBirth.getTime();
+
+//convert the calculated difference in date format  
+let age_dt = new Date(month_diff);
+
+//extract year from date      
+let year = age_dt.getUTCFullYear();
+
+//now calculate the age of the user  
+let age = Math.abs(year - 1970);
+
+document.getElementById("age").innerHTML = "(" + age + " years)"
